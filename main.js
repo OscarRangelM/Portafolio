1f1111f1139b09e1194f42345a910ee45cf0385c
OscarRangelM <orangelm1300@alumno.ipn.mx>
2022-09-20 23:25:33 -0500

    Creamos scroll container

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//menú hamburguesa
+
 const navToggle =document.querySelector(".varNav__toggle");
 const navMenu= document.querySelector(".varNav__sections");
 const firstChild= document.getElementById('firstChild');
@@ -10,3 +12,18 @@ navToggle.addEventListener("click",()=>{
     lastChild.classList.toggle("last");
     navMenu.classList.toggle("varNav__sections-visible");
 });
+
+
+// scroll container portfolio
+
+const prev= document.querySelector(".prev");
+const next= document.querySelector(".next");
+const slider= document.querySelector(".portfolio__gallery-cont");
+
+prev.addEventListener("click",()=>{
+    slider.scrollLeft -=300; //scrollLeft desplza a la izq
+})
+
+next.addEventListener("click",()=>{
+    slider.scrollLeft +=300; //scrollLeft desplza a la izq
+})
