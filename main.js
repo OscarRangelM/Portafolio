2e1667c84b52133c5836b8c1c9f30a05563d5229
OscarRangelM <orangelm1300@alumno.ipn.mx>
2022-10-14 00:07:40 -0500

    Agregamos documentacion

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 //menú hamburguesa
 
+/* Selecting the elements from the DOM. */
 const navToggle =document.querySelector(".varNav__toggle");
 const navMenu= document.querySelector(".varNav__sections");
 const firstChild= document.getElementById('firstChild');
 const secondChild= document.getElementById('secondChild');
 const lastChild= document.getElementById('lastChild');
 
+/* The above code is adding an event listener to the navToggle element. When the navToggle element is
+clicked, the firstChild, secondChild, and lastChild elements will toggle the class "first",
+"second", and "last" respectively. The navMenu element will toggle the class
+"varNav__sections-visible". */
 navToggle.addEventListener("click",()=>{
     firstChild.classList.toggle("first");
     secondChild.classList.toggle("second");
@@ -16,6 +21,7 @@ navToggle.addEventListener("click",()=>{
 
 // scroll container portfolio
 
+/* Selecting the elements from the DOM. */
 const prev= document.querySelector(".prev");
 const next= document.querySelector(".next");
 const slider= document.querySelector(".portfolio__gallery-cont");
